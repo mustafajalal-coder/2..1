d69cd8bf0d55ca6116560bb32273e7f7e453a071
muja07001 <muja07001@E-PW05CGR9>
2024-10-03 14:00:16 +0200

    Add project files.

---
+++ 2.44/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._11
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("vad är ditt favorit bok?");
+            string bok=Console.ReadLine();
+            Console.WriteLine("din favorit bok är " + bok );
+        }
+    }
+}
